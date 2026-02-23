4ebac75a0aa894816168861f2b37aa8dffdc1957
InnaMazur <ineska.lat@gmail.com>
2026-02-23 19:53:33 +0300

    HW - 12  and HW -13

--- react-netflix/src/App.jsx
+++ react-netflix/src/App.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Home from "./components/Home.jsx"
 import About from "./pages/About.jsx"
 import PricePage from "./pages/PricePage.jsx"
-import ContactPage from "./pages/ContactPage.jsx"
+import ContactsPage from "./pages/ContactsPage.jsx"
 
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
     <Route path='/' element={<Home></Home>}> </Route>
       <Route path='/about'element={<About></About>}></Route>
         <Route path='/price' element={<PricePage></PricePage>}> </Route>
-        <Route path='/contact' element={<ContactPage></ContactPage>}></Route>
+        <Route path='/contact' element={<ContactsPage></ContactsPage>}></Route>
         </Routes>
      </Router> 
     
